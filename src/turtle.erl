b5959f1d5182d6cccb231367e08499f0fb52336b
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-07-02 11:50:59 +0200

    Introduce tooling for declaring stuff in RabbitMQ and publishing messages.

--- src/turtle.erl
+++ src/turtle.erl
@@ -1,9 +1,51 @@
 -module(turtle).
+-include_lib("amqp_client/include/amqp_client.hrl").
 
--export([open_channel/1]).
+%% Low level API
+-export([
+	declare/2,
+	open_channel/1,
+	publish/4
+]).
+
+-type channel() :: pid().
 
 %% @doc open_channel/1 opens a channel on a given connection
 %% This function will return a channel on a given connection, bound in
 %% the system.
 %% @end
+-spec open_channel(atom()) -> {ok, channel()} | {error, Reason}
+    when Reason :: term().
 open_channel(Name) -> turtle_conn:open_channel(Name).
+
+%% @doc declare(Ch, Decls) declares a list of idempotent setup for the Channel/RabbitMQ
+%% The RabbitMQ declaration stack is nasty because it returns different results for different
+%% kinds of declarations. To fix this, we have this helper routine which will interpret
+%% the results of declarations.
+%%
+%% Returns `ok' on success, crashes if any of the declarations fail.
+%% @end
+-type declaration() :: #'exchange.declare'{} | #'queue.declare'{}.
+
+-spec declare(channel(), [declaration()]) -> ok.
+declare(_Channel, []) ->
+    ok;
+declare(Channel, [#'exchange.declare' {} = Exch | Ds]) ->
+    #'exchange.declare_ok'{} = amqp_channel:call(Channel, Exch),
+    declare(Channel, Ds);
+declare(Channel, [#'queue.declare' {} = Queue | Ds]) ->
+    #'queue.declare_ok'{} = amqp_channel:call(Channel, Queue),
+    declare(Channel, Ds);
+declare(Channel, [#'queue.bind' {} = Queue | Ds]) ->
+    #'queue.bind_ok'{} = amqp_channel:call(Channel, Queue),
+    declare(Channel, Ds).
+
+%% @doc publish(Chan, Exch, Key, Payload) publishes messages on a channel
+%% Low-level helper. Publish a `Payload' on the exchange `Exch' with routing key `Key'
+%% @end
+publish(Chan, X, Key, Payload) ->
+    Publish = #'basic.publish' {
+        exchange = X,
+        routing_key = Key
+    },
+    amqp_channel:cast(Chan, Publish, #amqp_msg { payload = Payload }).
