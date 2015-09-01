19538c33fced1d0b3b9d8202152c63eb8e939f3b
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-09-01 17:03:02 +0200

    Disable the unregistration of the confirm handler for now.

--- src/turtle_publisher.erl
+++ src/turtle_publisher.erl
@@ -90,7 +90,6 @@ init([Name, ConnName, Options]) ->
     %% the state with a #state{} record once that happens (see handle_info/2)
     Ref = gproc:nb_wait({n,l,{turtle,connection,ConnName}}),
     ok = exometer:ensure([ConnName, Name, casts], spiral, []),
-    process_flag(trap_exit, true),
     {ok, {initializing, Name, Ref, ConnName, Options}}.
 
 %% @private
@@ -170,7 +169,6 @@ handle_info(Info, State) ->
 
 %% @private
 terminate(_Reason, #state { channel = Channel }) when is_pid(Channel) ->
-    ok = amqp_channel:unregister_confirm_handler(Channel),
     ok;
 terminate(_Reason, _State) ->
     ok.
